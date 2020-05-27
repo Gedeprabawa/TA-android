76a248ec143f4ff8ebe04fab5bab5c15eee3a14c
Gedeprabawa <gedeprabawa88@gmail.com>
2020-05-28 01:22:18 +0800

    Edit Menu.java Class
    1. Melakukan perubahan pada public class yang akan digunakan untuk mengambil data untuk ditampilkan sesuai dengan layout dari item_menu.xml.

--- Projectjaringan/app/src/main/java/com/example/projectjaringan/Menu.java
+++ Projectjaringan/app/src/main/java/com/example/projectjaringan/Menu.java
@@ -1,28 +1,26 @@
 package com.example.projectjaringan;
 
-class Menu {
+public class Menu {
+
     private int gambar;
     private String nama;
-    private String Deskripsi;
-    private String pengertian;
-    private String fungsi;
-    private String cara_kerja;
-    private String harga;
+    private String deskripsi;
 
-    Menu() {
+    public Menu(int datagambar, String datanama,String datadeskripsi){
+        gambar = datagambar;
+        nama = datanama;
+        deskripsi = datadeskripsi;
     }
-    public int getGambar() { return gambar; }
-    public void setGambar(int gambar) { this.gambar = gambar; }
-    public String getNama() { return nama; }
-    public void setNama(String nama) { this.nama = nama; }
-    public String getDeskripsi() { return Deskripsi; }
-    public void setDeskripsi(String deskripsi) { Deskripsi = deskripsi; }
-    public String getPengertian() { return pengertian; }
-    public void setPengertian (String pengertian) { this.pengertian = pengertian; }
-    public String getFungsi() { return fungsi; }
-    public void setFungsi(String fungsi) {this.fungsi= fungsi; }
-    public String getCara_kerja() { return cara_kerja; }
-    public void setCara_kerja(String cara_kerja) { this.cara_kerja = cara_kerja; }
-    public String getHarga() { return harga; }
-    public void setHarga(String harga) { this.harga = harga; }
+
+    public int getGambar() {
+        return gambar;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
 }
