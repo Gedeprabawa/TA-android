2e5650da90263be1c16ccf419e89ef1b977b9725
Gedeprabawa <gedeprabawa88@gmail.com>
2020-06-03 21:40:38 +0800

    Mengedit Menu Adapter
    Melakukan edit MenuAdapter.java, untuk menghubungkan pengambilan data dari database website yang sudah dibuat.

--- Projectjaringan/app/src/main/java/com/example/projectjaringan/MenuAdapater.java
+++ Projectjaringan/app/src/main/java/com/example/projectjaringan/MenuAdapater.java
@@ -1,6 +1,7 @@
 package com.example.projectjaringan;
 
 import android.content.Context;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,61 +9,67 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 
-public class MenuAdapater extends BaseAdapter {
-    private Context context;
-    private ArrayList<Menu> menus= new ArrayList<>();
 
-    public void setMenus(ArrayList<Menu> menus) {
-        this.menus = menus;
+public class MenuAdapater extends RecyclerView.Adapter<MenuAdapater.MenuViewHolder> {
+
+
+    private Context context;
+    private ArrayList<Menu> menus;
+    public MenuAdapater(Context mcontext, ArrayList<Menu> menujar){
+        context= mcontext;
+        menus= menujar;
+
     }
 
-    public MenuAdapater(Context context) {
-        this.context = context;
+    @NonNull
+    @Override
+    public MenuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item_menu,parent,false);
+
+        return new MenuViewHolder(v);
     }
 
     @Override
-    public int getCount() {
+    public void onBindViewHolder(@NonNull MenuViewHolder holder, int position) {
+        Menu menubaru = menus.get(position);
+        int gambarbaru = menubaru.getGambar();
+        String namabaru = menubaru.getNama();
+        String deskripsibaru = menubaru.getDeskripsi();
+
+        holder.tvdatanama.setText(namabaru);
+        holder.tvdatades.setText(deskripsibaru);
+        Glide
+                .with(context)
+                .load(gambarbaru)
+                .centerCrop()
+                .into(holder.ivdataimg);
+    }
+
+    @Override
+    public int getItemCount() {
         return menus.size();
     }
 
-    @Override
-    public Object getItem(int i) {
-        return menus.get(i);
-    }
+    public class MenuViewHolder extends RecyclerView.ViewHolder {
+        public ImageView ivdataimg;
+        public TextView tvdatanama;
+        public TextView tvdatades;
+        public MenuViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivdataimg = itemView.findViewById(R.id.img_menu);
+            tvdatanama = itemView.findViewById(R.id.tv_menu);
+            tvdatades = itemView.findViewById(R.id.tv_deskripsi);
 
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        View itemView= view;
-        if (itemView== null){
-            itemView= LayoutInflater.from(context).inflate(R.layout.item_menu,viewGroup,false);
 
         }
-        ViewHoleder viewholeder = new ViewHoleder(itemView);
-        Menu menu= (Menu)getItem(i);
-        viewholeder.bind(menu);
-        return itemView;
     }
-    private class ViewHoleder{
-        private ImageView gambar;
-        private TextView tvnama;
-        private TextView tvdes;
 
-        ViewHoleder(View view){
-            gambar=view.findViewById(R.id.img_menu);
-            tvnama=view.findViewById(R.id.tv_menu);
-            tvdes=view.findViewById(R.id.tv_des);
-        }
-        void bind(Menu menu){
-            gambar.setImageResource(menu.getGambar());
-            tvnama.setText(menu.getNama());
-            tvdes.setText(menu.getDeskripsi());
-        }
-    }
+
 }
